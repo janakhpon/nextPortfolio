6ecdf9130875de8a9f37834aabc71eb194a00f77
janakhpon <jnovaxer@gmail.com>
2020-01-17 20:04:52 +0630

    GDEV000

---
+++ pages/cv.js
@@ -0,0 +1,7 @@
+const cv = () => {
+    return (
+        <h1>cv</h1>
+    )
+}
+
+export default cv
---
+++ pages/portfolios.js
@@ -0,0 +1,7 @@
+const portfolios = () => {
+    return (
+        <h1>portfolios</h1>
+    )
+}
+
+export default portfolios
